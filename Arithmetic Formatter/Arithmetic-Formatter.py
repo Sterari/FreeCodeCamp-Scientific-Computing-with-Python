ebd09027a5ea5b4117fce0a158ef58a3b9d8b104
sterari <aritz.sterne@gmail.com>
2022-08-22 17:21:16 +0100

    re-formatting + naming standards

--- Arithmetic Formatter/Arithmetic-Formatter.py
+++ Arithmetic Formatter/Arithmetic-Formatter.py
@@ -1,73 +1,101 @@
-def arithmetic_arranger(prob, soln=False):
+def arithmetic_arranger(problem_list, solution=False):
+
     # returns an error if the user inputs more than 5 problems
-    if len(prob) > 5:
+    if len(problem_list) > 5:
         return "Error: Too many problems."
 
     # returns an error if the user inputs a non-numerical operand
-    for x in prob:
-        x = x.split()
+    for problem in problem_list:
+        problem = problem.split()
         try:
-            x[0] = int(x[0])
-            x[2] = int(x[2])
+            problem[0] = int(problem[0])
+            problem[2] = int(problem[2])
         except:
             return "Error: Numbers must only contain digits."
 
-    sollist = []
-    operandUP = []
-    operandDOWN = []
+    solution_list = []
+    upper_operands = []
+    lower_operands = []
     operators = []
 
     # creating 3 lists: 1 for upper operands, 1 for lower operands and 1 for operators
-    for x in prob:
-        x = x.split()
-        operandUP = operandUP + [x[0]]
-        operators = operators + [x[1]]
-        operandDOWN = operandDOWN + [x[2]]
+    for problem in problem_list:
+        problem = problem.split()
+        upper_operands += [problem[0]]
+        operators += [problem[1]]
+        lower_operands += [problem[2]]
 
         # returning error if len of operand > 4
-        if len(x[0]) > 4 or len(x[2]) > 4:
+        if len(problem[0]) > 4 or len(problem[2]) > 4:
             return "Error: Numbers cannot be more than four digits."
-
 
         # calculating solutions to problems
         else:
-            if x[1] == '+':
-                y = int(x[0]) + int(x[2])
-            elif x[1] == '-':
-                y = int(x[0]) - int(x[2])
+            if problem[1] == "+":
+                y = int(problem[0]) + int(problem[2])
+            elif problem[1] == "-":
+                y = int(problem[0]) - int(problem[2])
 
             # returning error if an operator is not a + or -
             else:
                 return "Error: Operator must be '+' or '-'."
 
-            sollist = sollist + [y]
+            solution_list += [y]
 
-    UPstr = str()
-    DOWNstr = str()
-    solstr = str()
-    dashstr = str()
+    upper_str = str()
+    lower_str = str()
+    solution_str = str()
+    dash_str = str()
 
     # creating dash string
-    for z in range(0, len(prob)):
-        dashstr = dashstr + str((max(len(operandUP[z]), len(operandDOWN[z])) + 2) * "-" + 4 * " ")
+    for z in range(0, len(problem_list)):
+        dash_str = dash_str + str(
+            (max(len(upper_operands[z]), len(lower_operands[z])) + 2) * "-" + 4 * " "
+        )
 
     # converting dash string into dash list which will be used as a basis to format the strings of operands and solutions
-    dashlist = list(dashstr.split())
+    dash_list = list(dash_str.split())
 
     # creating formatted strings of upper and lower operands, as well as the solutions
-    for a in range(0, len(prob)):
-        solstr = solstr + (len(dashlist[a]) - len(str(sollist[a]))) * " " + str(sollist[a]) + 4 * " "
-        UPstr = UPstr + (len(dashlist[a]) - len(operandUP[a])) * " " + operandUP[a] + 4 * " "
-        if len(operandUP[a]) < len(operandDOWN[a]):
-            DOWNstr = DOWNstr + operators[a] + " " + operandDOWN[a] + 4 * " "
+    for a in range(0, len(problem_list)):
+        solution_str = (
+            solution_str
+            + (len(dash_list[a]) - len(str(solution_list[a]))) * " "
+            + str(solution_list[a])
+            + 4 * " "
+        )
+        upper_str = (
+            upper_str
+            + (len(dash_list[a]) - len(upper_operands[a])) * " "
+            + upper_operands[a]
+            + 4 * " "
+        )
+        if len(upper_operands[a]) < len(lower_operands[a]):
+            lower_str = lower_str + operators[a] + " " + lower_operands[a] + 4 * " "
         else:
-            DOWNstr = DOWNstr + operators[a] + (len(operandUP[a]) - len(operandDOWN[a]) + 1) * " " + operandDOWN[
-                a] + 4 * " "
+            lower_str = (
+                lower_str
+                + operators[a]
+                + (len(upper_operands[a]) - len(lower_operands[a]) + 1) * " "
+                + lower_operands[a]
+                + 4 * " "
+            )
 
     # creating string of arranged problems
-    if soln == True:
-        arranged_problems = UPstr.rstrip() + '\n' + DOWNstr.rstrip() + "\n" + dashstr.rstrip() + "\n" + solstr.rstrip()
+    if solution is True:
+        arranged_problems = (
+            upper_str.rstrip()
+            + "\n"
+            + lower_str.rstrip()
+            + "\n"
+            + dash_str.rstrip()
+            + "\n"
+            + solution_str.rstrip()
+        )
     else:
-        arranged_problems = UPstr.rstrip() + "\n" + DOWNstr.rstrip() + "\n" + dashstr.rstrip()
+        arranged_problems = (
+            upper_str.rstrip() + "\n" + lower_str.rstrip() + "\n" + dash_str.rstrip()
+        )
 
     return arranged_problems
+
